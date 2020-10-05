f8b9196a8ea2eb4b9571bcb79eeefd51a0ff49a3
EzeScigo <scigolini@gmail.com>
2020-10-05 23:12:26 +0200

    adding shop page, collection preview component and collection item comp.

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route } from 'react-router';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+
+import './shop.data.js';
+import SHOP_DATA from './shop.data.js';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        const {collections} = this.state;
+        return (
+            <div className='shop-page'>
+                {
+                    collections.map(({id, ...otherCollectionProps}) => (
+                        <CollectionPreview key={id} {...otherCollectionProps} />
+                    ))
+                }
+            </div>
+            
+        )
+    }
+}
+
+export default ShopPage;
