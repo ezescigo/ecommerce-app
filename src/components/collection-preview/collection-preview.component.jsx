e9b1a521c333ff5bccf83dd038432ec098448ebd
EzeScigo <scigolini@gmail.com>
2020-10-19 13:53:58 +0200

    Added slider component on collection-preview. Fixed icon on navbar and adjusted items footers

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,19 +2,32 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component.jsx';
 
+import { Slider } from '@lifarl/react-scroll-snap-slider';
+
 import './collection-preview.styles.scss';
+
+const slidesPerPageSettings = {
+  mobileSmall: 1.5,
+  mobileBig: 2.5,
+  tablet: 4,
+  desktop: 4,
+}
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {items
-        .filter((item, index) => index < 4)
-        .map(item => (
-          <CollectionItem key={item.id} item={item} />
-      ))}
+      <Slider
+      slidesPerPageSettings={slidesPerPageSettings}>
+        {items
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
+        ))}
+      </Slider>
     </div>
   </div>
 );
 
 export default CollectionPreview;
+
+// .filter((item, index) => index < 4)
